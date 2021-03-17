525c330e877dbe321cc3cf50d418f74cffbee217
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-17 14:29:15 -0400

    add starting pages

---
+++ gatsby/.cache/sync-requires.js
@@ -0,0 +1,16 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Jonathan Gomez\\Desktop\\pizza\\gatsby\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Jonathan Gomez\\Desktop\\pizza\\gatsby\\src\\pages\\404.js"))),
+  "component---src-pages-beers-js": hot(preferDefault(require("C:\\Users\\Jonathan Gomez\\Desktop\\pizza\\gatsby\\src\\pages\\beers.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Jonathan Gomez\\Desktop\\pizza\\gatsby\\src\\pages\\index.js"))),
+  "component---src-pages-orders-js": hot(preferDefault(require("C:\\Users\\Jonathan Gomez\\Desktop\\pizza\\gatsby\\src\\pages\\orders.js"))),
+  "component---src-pages-pizzas-js": hot(preferDefault(require("C:\\Users\\Jonathan Gomez\\Desktop\\pizza\\gatsby\\src\\pages\\pizzas.js"))),
+  "component---src-pages-slicemaster-js": hot(preferDefault(require("C:\\Users\\Jonathan Gomez\\Desktop\\pizza\\gatsby\\src\\pages\\slicemaster.js")))
+}
+
---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function HomePage() {
+  return <div>
+    <p>Hey! I'm the home page</p>
+  </div>
+}
