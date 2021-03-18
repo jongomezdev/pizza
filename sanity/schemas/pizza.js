1a34401cadeeae068cbd9a6c376758ad0c0b0414
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-18 16:20:28 -0400

    create data relationships

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -39,5 +39,33 @@ export default {
       validation: (Rule) => Rule.min(1000),
       // TODO: Add custom input component
     },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // 1. Filter undefined toppings out
+      const tops = Object.values(toppings).filter(Boolean);
+      // prevents prettier from returning an implicent return
+      console.log('STAY');
+      // 2. return the preview object for the pizza
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
