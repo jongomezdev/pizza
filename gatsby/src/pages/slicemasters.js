ce619ad2370fe93cda77ff377038e80814656144
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-21 13:05:18 -0400

    render slicemaster data

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import Nav from '../components/Nav';
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
 
-export default function SlicemastersPage() {
+export default function SlicemastersPage({ data }) {
+  const slicemasters = data.slicemasters.nodes;
+  console.log(slicemasters);
   return (
     <>
-      <p>Hey! I'm the Slicemasters page</p>
+      <div>
+        {slicemasters.map((person) => (
+          <div>
+            <Link to={`/slicemaster/${person.slug.current}`}>
+              <h2>
+                <span className="mark">{person.name}</span>
+              </h2>
+            </Link>
+            <Img fluid={person.image.asset.fluid} />
+            <p className="description">{person.description}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        description
+        image {
+          asset {
+            fluid(maxWidth: 410) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
