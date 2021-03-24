6efab2a774a79b1012eaeff5af8ffa2c5a82143f
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-24 14:27:05 -0400

    create useForm custom hook

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SEO from '../components/SEO';
 
 export default function OrderPage() {
+  const [name, setName] = useState('');
+
   return (
     <>
       <SEO title="Order a pizza!" />
-      <p>Hey! I'm the Orders page</p>
+      <form>
+        <fieldset>
+          <legend>Your info</legend>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" />
+        </fieldset>
+        <fieldset>
+          <legend>Menu</legend>
+        </fieldset>
+        <fieldset>
+          <legend>Order</legend>
+        </fieldset>
+      </form>
     </>
   );
 }
