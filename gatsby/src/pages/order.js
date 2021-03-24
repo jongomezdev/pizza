3d32ef9ab71927f5ea2e62751711bbfda0f73f06
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-24 15:11:37 -0400

    create pizza query on order page and render the data, make buttons for pizza size

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
 
-export default function OrderPage() {
-  const [name, setName] = useState('');
-
+export default function OrderPage({ data }) {
+  // const [name, setName] = useState('');
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+  });
+  const pizzas = data.pizzas.nodes;
   return (
     <>
       <SEO title="Order a pizza!" />
@@ -14,14 +21,39 @@ export default function OrderPage() {
           <input
             type="text"
             name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            id="name"
+            value={values.name}
+            onChange={updateValue}
           />
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" />
+          <input
+            type="text"
+            name="email"
+            id="email"
+            value={values.email}
+            onChange={updateValue}
+          />
         </fieldset>
         <fieldset>
           <legend>Menu</legend>
+          {pizzas.map((pizza) => (
+            <div key={pizza.id}>
+              <Img
+                width="50"
+                height="50"
+                fluid={pizza.image.asset.fluid}
+                alt={pizza.name}
+              />
+              <div>
+                <h2>{pizza.name}</h2>
+              </div>
+              <div>
+                {['S', 'M', 'L'].map((size) => (
+                  <button type="button">{size}</button>
+                ))}
+              </div>
+            </div>
+          ))}
         </fieldset>
         <fieldset>
           <legend>Order</legend>
@@ -30,3 +62,25 @@ export default function OrderPage() {
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        price
+        image {
+          asset {
+            fluid(maxWidth: 100) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
